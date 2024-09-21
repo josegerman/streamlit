8d5b3c20723d2c9906d4073920e89e88c458298f
JoseG <josegerman@users.noreply.github.com>
2024-09-21 11:13:46 -0400

    Update streamlit_rag_app.py

--- streamlit_rag_app.py
+++ streamlit_rag_app.py
@@ -6,9 +6,9 @@
 # ==================================
 # Imports
 # ==================================
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import os
 
